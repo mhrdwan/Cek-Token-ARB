d76187569040e98962a7035b0b24fb4783fd432f
mhrdwan <futuremhr@gmail.com>
2023-05-14 13:43:49 +0700

    2

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 - npm install
 - lalu ke src/scanner/Index.js 
-- ganti YOUR-API-KEY dengan api-key dari https://moralis.io/
+- ganti const apikey = ` api-key  `; dengan api-key dari https://moralis.io/
 - npm start
--- src/scanner/Index.js
+++ src/scanner/Index.js
@@ -6,8 +6,15 @@ import { Row, Col, Form, Button } from "react-bootstrap";
 function Page1() {
   const [isidataapi, setIsidaApi] = useState([]);
   const [inputValue, setInputValue] = useState("");
+  const [isiduit, setIsiduit] = useState("");
   const [errorMessage, setErrorMessage] = useState(null);
+  const apikey = `api-key`;
   const columns = [
+    {
+      name: "No",
+      selector: (row) => row.no,
+      width: `50px`,
+    },
     {
       name: "Nama Token",
       selector: (row) => row.name,
@@ -36,20 +43,19 @@ function Page1() {
         `https://deep-index.moralis.io/api/v2/${inputValue}/erc20?chain=arbitrum&addres`,
         {
           headers: {
-            "X-API-Key":
-              "YOUR-API-KEY",
+            "X-API-Key": apikey,
           },
         }
       );
 
-      const data = isi.data.map((item) => ({
+      const data = isi.data.map((item , index) => ({
+        no : index + 1,
         name: item.name,
         symbol: item.symbol,
         balance: (item.balance / Math.pow(10, 18)).toFixed(2),
         token_address: item.token_address,
       }));
 
-      console.log(`isi`, data);
       setIsidaApi(data);
       setInputValue("");
       setErrorMessage(null);
@@ -62,16 +68,33 @@ function Page1() {
     }
   };
 
-  useEffect(() => {
-    // datatoken();
-  }, []);
+  const balance = async () => {
+    const isi = await axios.get(
+      `https://deep-index.moralis.io/api/v2/wallets/balances?chain=arbitrum&wallet_addresses=${inputValue}`,
+      {
+        headers: {
+          "X-API-Key": apikey,
+        },
+      }
+    );
+
+    const isiw =
+      isi.data?.[0].wallet_balances?.map((item, index) => ({
+        address: item.address,
+        balance_formatted: item.balance_formatted,
+      })) || [];
+    setIsiduit(isiw);
+
+    console.log(isiw);
+  };
 
   const handleButtonClick = () => {
     datatoken(inputValue);
+    balance(inputValue);
   };
   return (
     <div>
-      <h1>Cek Token Apa Saja Di Wallet ARB</h1>
+      <h1>Cek Token Di Wallet ARB Mu</h1>
       <br />
       <Row>
         <div className="d-flex justify-content-center">
@@ -80,22 +103,29 @@ function Page1() {
               <Form.Group className="mb-3" controlId="formInput">
                 <Form.Control
                   type="text"
-                  placeholder="Enter text"
+                  placeholder="Masukkan Address ARB 0x"
                   value={inputValue}
                   onChange={(e) => setInputValue(e.target.value)}
                 />
               </Form.Group>
+
+              <h5>Address Kamu Adalah</h5>
+              {isiduit[0]?.address}
+              <h5>
+                <br />
+                Saldo ARB
+              </h5>
+              {isiduit[0]?.balance_formatted}
+              <br />
               <Button variant="primary" onClick={handleButtonClick}>
                 Submit
               </Button>
-              {errorMessage && (
-                <p style={{ color: "red" }}>{errorMessage}</p> 
-              )}
+              {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
             </Form>
           </Col>
         </div>
         <Col sm={{ span: 8, offset: 2 }}>
-          <DataTable columns={columns} data={isidataapi} />
+          <DataTable columns={columns} data={isidataapi} pagination />
         </Col>
       </Row>
     </div>
